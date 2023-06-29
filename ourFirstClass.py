2db8ad61b454eef0225f203ce076f7f843533162
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-29 00:43:18 -0500

    declaring types for arguments

--- ourFirstClass.py
+++ ourFirstClass.py
@@ -1,15 +1,17 @@
 class Item:
-    def __init__(self, name, price, quantity):
+    def __init__(self, name: str, price: float, quantity=0): # you declare types for your arguments to ensure correct objects goes into the variable,  
+  
         self.name = name
         self.price = price
         self.quantity = quantity
-    def calcTotalPrice(self, x, y):
-        return x * y
+    
+    
+    def calcTotalPrice(self):
+        return self.price * self.quantity
 
 item1 = Item("Phone", 100, 5)
 
 item2 = Item("Laptop", 1000, 3)
 
 
-print(item1.quantity)
-print(item2.name)
+print(item1.calcTotalPrice())
