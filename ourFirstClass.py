42b21ef5e9f6bb4175c71268c78413c639c4f07e
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-28 20:13:44 -0500

    methods to create instances of variables i think

--- ourFirstClass.py
+++ ourFirstClass.py
@@ -1,20 +1,15 @@
 class Item:
-    def __init__(self):
-        print("I am created!")
-        
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
     def calcTotalPrice(self, x, y):
         return x * y
 
-item1 = Item()
+item1 = Item("Phone", 100, 5)
 
-item1Name = "Phone"
-item1Price = 100
-item1Quantity = 5
+item2 = Item("Laptop", 1000, 3)
 
-print(item1.calcTotalPrice(item1Price, item1Quantity))
 
-item1 = Item()
-
-item1Name = "Phone"
-item1Price = 100
-item1Quantity = 5
+print(item1.quantity)
+print(item2.name)
